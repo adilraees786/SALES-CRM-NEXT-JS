01a34662348f86089d92ec0ddc75b66ee399af72
Adil Raees <adilraees911@gmail.com>
2025-09-17 20:10:21 +0500

    fixed

--- components/reuseable/Button.jsx
+++ components/reuseable/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ children, onClick, extraClass }) => {
   return (
     <button
       onClick={onClick}
-      className={`px-5 py-2 bg-[#1689FE] text-white font-semibold rounded-md hover:bg-primary-hover transition whitespace-nowrap ${extraClass}`}
+      className={`px-5 py-2 bg-[#1689FE] text-white font-semibold rounded-md hover:bg-primary-hover transition whitespace-nowrap ${extraClass}` }
     >
       {children}
     </button>
